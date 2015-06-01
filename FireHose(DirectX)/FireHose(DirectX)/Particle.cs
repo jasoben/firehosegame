75c7239f2e069c6be2c8299ffd6b94d5da86ac39
jasoben80 <jasoben@gmail.com>
2015-05-31 21:49:18 -0400

    Alpha DirectX Build

---
+++ FireHose(DirectX)/FireHose(DirectX)/Particle.cs
@@ -0,0 +1,61 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using FarseerPhysics.Dynamics;
+using FarseerPhysics.Factories;
+using FarseerPhysics.Collision;
+using FarseerPhysics.Common;
+using FarseerPhysics.Controllers;
+using FarseerPhysics.Dynamics.Contacts;
+
+#endregion
+namespace FireHose_DirectX_
+{
+    class Particle
+    {
+
+        public Texture2D ParticleTexture { get; set; }
+        public Vector2 ParticlePosition { get; set; }
+        public Vector2 ParticleVelocity { get; set; }
+        
+        //public float FireAngle { get; set; }
+        //public float FireAngularVelocity { get; set; }
+        public Color ParticleColor { get; set; }
+        public float ParticleSize { get; set; }
+        public int ParticleTTL { get; set; }
+
+        public Particle(Texture2D particleTexture, Vector2 particlePosition, Vector2 particleVelocity, Color particleColor, float particleSize, int particleTTL)
+        {
+
+            ConvertUnits.SetDisplayUnitToSimUnitRatio(64f);
+            ParticlePosition = ConvertUnits.ToDisplayUnits(particlePosition);
+            ParticleTexture = particleTexture;
+            ParticleColor = particleColor;
+            ParticleSize = particleSize;
+            ParticleTTL = particleTTL;
+            ParticleVelocity = new Vector2(particleVelocity.X, -1 * particleVelocity.Y);
+            ParticleVelocity = ParticleVelocity / 5;
+        }
+
+        public void Update()
+        {
+            ParticleTTL--;
+            ParticlePosition += ParticleVelocity;
+            
+        }
+
+        
+        public void Draw(SpriteBatch particleSB)
+        {
+            Rectangle sourceRectangle = new Rectangle(0, 0, ParticleTexture.Width, ParticleTexture.Height);
+            Vector2 origin = new Vector2(ParticleTexture.Width / 2, ParticleTexture.Height / 2);
+            particleSB.Draw(ParticleTexture, ParticlePosition, sourceRectangle, ParticleColor, 1f, origin, ParticleSize, SpriteEffects.None, 0f);
+           
+        }
+    }
+}
---
+++ FireHose(DirectX)/FireHose(DirectX)/WaterGun.cs
@@ -0,0 +1,102 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using FarseerPhysics.Dynamics;
+using FarseerPhysics.Factories;
+using FarseerPhysics.Collision;
+using FarseerPhysics.Common;
+using FarseerPhysics.Controllers;
+using FarseerPhysics.Dynamics.Contacts;
+
+#endregion
+
+namespace FireHose_DirectX_
+{
+    class WaterGun
+    {
+
+        //Player player;
+        public Vector2 PlayerPosition;
+        
+
+        public Vector2 GunOrigin;
+        public float GunRotation;
+
+        Texture2D waterGunTexture;
+        Texture2D waterDropTexture; 
+        ParticleEngine waterParticleEngine;
+        
+
+        World world;
+
+        public bool isFiring = false;
+
+        public WaterGun(Player player, Vector2 playerPosition, World world)
+        {
+            ConvertUnits.SetDisplayUnitToSimUnitRatio(64f);
+            PlayerPosition = playerPosition;
+
+            this.world = world;
+            
+            
+        }
+
+        public void LoadContent(ContentManager content)
+        {
+            waterGunTexture = content.Load<Texture2D>("watergun.png");
+            waterDropTexture = content.Load<Texture2D>("particle.png");
+            waterParticleEngine = new ParticleEngine(world, waterDropTexture, PlayerPosition, Color.Orange);
+                
+        }
+
+        public void Update(Vector2 playerPosition, Controls controls, List<Keys> playerControls)
+        {
+            PlayerPosition = playerPosition;
+            BlastWater(controls, playerControls);
+            if (isFiring == false)
+            {
+                waterParticleEngine.Update(isFiring);
+            }
+
+            GunOrigin = controls.Fly() * 10;
+            GunOrigin = new Vector2(GunOrigin.X, -1 * GunOrigin.Y);
+            GunRotation = (float)(Math.Atan2(GunOrigin.Y, GunOrigin.X));
+
+            //testing
+
+            
+        }
+
+        public void Draw(SpriteBatch sb)
+        {
+            sb.Draw(waterGunTexture, ConvertUnits.ToDisplayUnits(PlayerPosition), null, Color.White, GunRotation, new Vector2 (0,0), .2f, SpriteEffects.None, 0f);
+            waterParticleEngine.Draw(sb);
+        }
+
+        public void BlastWater(Controls controls, List<Keys> playerControls)
+        {
+            if (controls.isThumbStick(Buttons.RightThumbstickDown) || controls.isThumbStick(Buttons.RightThumbstickUp) || controls.isThumbStick(Buttons.RightThumbstickLeft) || controls.isThumbStick(Buttons.RightThumbstickRight))
+            {
+               isFiring = true;
+                waterParticleEngine.ParticleEmitterLocation = PlayerPosition;
+                waterParticleEngine.ParticleEmitterVelocity = controls.Fly();
+                waterParticleEngine.Update(isFiring);
+                
+                
+            } else         
+            {
+                isFiring = false;
+                waterParticleEngine.ParticleEmitterLocation = PlayerPosition;
+                waterParticleEngine.ParticleEmitterVelocity = controls.Fly();
+                waterParticleEngine.Update(isFiring);
+                
+            }
+
+        }
+    }
+}
